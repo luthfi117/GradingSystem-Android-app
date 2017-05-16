addc74c95fb4287664e48fd7492bc46c64d81dc4
luthfi117 <luthfi.nurhakim@gmail.com>
2017-05-16 20:18:40 +0700

    first commit

---
+++ app/src/main/java/com/example/maslu/gradesystem/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.maslu.gradesystem;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login_home(View v){
+        Intent i = new Intent(this, home.class);
+        startActivity(i);
+    }
+}
